eb62a7c096774c760ebd0cbb6f38c43697af0ac8
Zhi Heng <25006645@siswa.um.edu.my>
2026-04-03 17:33:19 +0800

    Remove bugs

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -4,9 +4,16 @@ importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-comp
 firebase.initializeApp({
   apiKey: "AIzaSyAGfu6muyvZQxiD-i3FBcnVzi62UPijBSE",
   authDomain: "careloop-b2ec8.firebaseapp.com",
-  projectId: "careloop-b2ec8,
+  projectId: "careloop-b2ec8",
   messagingSenderId: "362769739395",
   appId: "1:362769739395:web:44cf4c44059886e25df5b6"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/icons/Icon-192.png'
+  });
+});
